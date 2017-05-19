78c4d14adef574424e3f397f4e779c65aef9abc0
Rajmund Staniek <rajmund.st@gmail.com>
2017-05-20 01:25:08 +0200

    created test for spead comparison between sequential and concurrent implementation

---
+++ tests/info/rajmundstaniek/TcCrypt/TcDigestTestComparison.java
@@ -0,0 +1,41 @@
+package info.rajmundstaniek.TcCrypt;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static info.rajmundstaniek.TcCrypt.TcDigest.ActionType;
+import static info.rajmundstaniek.TcCrypt.TcDigest.DigestSystem;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rajmu on 17.05.20.
+ */
+public class TcDigestTestComparison {
+
+    private TcDigest tcDigest;
+    private final ActionType actionType = ActionType.ENCODE;
+    private final DigestSystem system = DigestSystem.BIN;
+
+    @Before
+    public void setUp() throws Exception {
+        tcDigest = new TcDigest();
+    }
+
+    @Test
+    public void sequential() throws Exception {
+        tcDigest.processData(TxtRes.longAssText9000, "okon", actionType, system);
+    }
+
+    @Test
+    public void concurrent() throws Exception {
+        tcDigest.processParallel(TxtRes.longAssText9000, "okon", actionType, system);
+    }
+
+    @Test
+    public void integrityCheck() throws Exception {
+        String parallel = tcDigest.processParallel(TxtRes.longAssText9000, "okon", actionType, system);
+        String sequential = tcDigest.processData(TxtRes.longAssText9000, "okon", actionType, system);
+
+        assertEquals(parallel, sequential);
+    }
+}
